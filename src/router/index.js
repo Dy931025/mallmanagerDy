07887de2164a1e3437bb241d5c18b417a03d1e97
unknown <243478051@qq.com>
2018-11-28 00:40:55 +0800

    删除了一些东洗

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login.vue'
 import Home from '@/components/home/home.vue'
-<<<<<<< HEAD
-import Users from '@/components/users/users.vue'
-=======
 import User from '@/components/user/user.vue'
->>>>>>> dev-user
 Vue.use(Router)
 
 export default new Router({
@@ -20,21 +16,11 @@ export default new Router({
       name: 'home',
       path: '/',
       component: Home,
-<<<<<<< HEAD
-      children:[
-        {
-          name:'users',
-          path:'users',
-          component:Users
-        }
-      ]
-=======
       children:[{
         name:'user',
         path:'user',
         component:User
       }]
->>>>>>> dev-user
     }
   ]
 })
